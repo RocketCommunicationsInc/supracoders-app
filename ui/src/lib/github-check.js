1100b9d21a7aba68e2e03aa5a341df6f3227e2a9
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-02-22 13:59:35 -0500

    see if we can get default data on deploy preview

--- ui/src/lib/github-check.js
+++ ui/src/lib/github-check.js
@@ -1,3 +1,7 @@
 export const githubCheck = () => {
-  return window.location.hostname.includes('github') || window.location.hostname === 'localhost';
+  return (
+    window.location.hostname.includes('github') ||
+    window.location.hostname.includes('netlify') ||
+    window.location.hostname === 'localhost'
+  );
 };
