2de423bc92a4787cde69e4f648644abe10a1386c
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-03-15 10:24:10 -0400

    feet

--- ui/src/components/Footer/Footer.jsx
+++ ui/src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-// import { AstroTheme } from '../../themes/AstroTheme';
 import { Box, Link } from '@mui/material';
 
 export const Footer = () => (
@@ -42,26 +41,6 @@ export const Footer = () => (
         </Link>
         .
       </Typography>
-      {/* <Typography>
-        Authors: Askins,{' '}
-        <Link href='https://github.com/filmo003' target='_new'>
-          Gilmore
-        </Link>
-        ,{' '}
-        <Link href='https://github.com/bjhufstetler' target='_new'>
-          Hufstetler
-        </Link>
-        ,{' '}
-        <Link href='https://github.com/thkruz/' target='_new'>
-          Kruczek
-        </Link>
-        ,{' '}
-        <Link href='https://github.com/bigbpete' target='_new'>
-          Peters
-        </Link>
-        <br></br>
-        United States Space Force Supra Coders: Blended Software Development Immersion #1
-      </Typography> */}
     </Toolbar>
   </Box>
 );
