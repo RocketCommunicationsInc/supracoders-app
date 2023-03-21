4f55cf6b014dd7b963e2ed1e3600bb77d7d3e54d
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-03-21 16:06:44 -0400

    icon additions

--- ui/src/AppIcons.jsx
+++ ui/src/AppIcons.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RuxIconAntenna, RuxIconLockOpen, RuxIconLock, RuxIconHelp, RuxIconWarning, RuxIconSignalCellularAlt, RuxIconAntennaTransmit, RuxIconExitToApp, RuxIconClose, RuxIconWbSunny, RuxIconBrightness3, RuxIconLoop, RuxIconSettings, RuxIconPlayArrow, RuxIconPause } from '@astrouxds/react/dist/components';
+import { RuxIconAntenna, RuxIconLockOpen, RuxIconLock, RuxIconHelp, RuxIconWarning, RuxIconSignalCellularAlt, RuxIconAntennaTransmit, RuxIconExitToApp, RuxIconClose, RuxIconWbSunny, RuxIconBrightness3, RuxIconLoop, RuxIconSettings, RuxIconPlayArrow, RuxIconPause, RuxIconArrowDropUp, RuxIconArrowDropDown, RuxIconPerson } from '@astrouxds/react/dist/components';
 export const AppIcons = () => {
     return (
             <span style={{display: 'none'}}>
@@ -18,6 +18,9 @@ export const AppIcons = () => {
                 <RuxIconSettings />
                 <RuxIconPlayArrow />
                 <RuxIconPause />
+                <RuxIconArrowDropUp />
+                <RuxIconArrowDropDown />
+                <RuxIconPerson />
             </span>
         )
 }
